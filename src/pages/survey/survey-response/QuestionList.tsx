2c7da2feb6a63870a6398e777826e0c19a41bd17
limhizy15 <limhizy15@ajou.ac.kr>
2024-08-07 17:19:29 +0900

    refactor: QuestionList 컴포넌트 분리

--- src/pages/survey/survey-response/QuestionList.tsx
+++ src/pages/survey/survey-response/QuestionList.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { SurveyQuestion } from '../../../app/survey/types/survey-question';
 import * as styles from './styles.css';
+import { SurveyQuestion } from '../../../app/survey/types/survey-question';
 
 interface QuestionListProps {
   questions: SurveyQuestion[];
@@ -17,68 +17,98 @@ export function QuestionList({
   touched,
   errors,
 }: QuestionListProps) {
-  return (
-    <>
-      {questions.map((question) => (
-        <div key={question.id} className={styles.questionContainer}>
-          <label htmlFor={question.id} className={styles.questionTitle}>
-            {question.title}
-            {question.required && (
-              <span className={styles.requiredStar}>*</span>
-            )}
-          </label>
-          {question.type === 'text' && (
-            <input
-              {...getFieldProps(question.id, { type: 'text' })}
-              className={styles.textInput}
-            />
-          )}
-          {question.type === 'textarea' && (
-            <textarea
-              {...getFieldProps(question.id, { type: 'textarea' })}
-              className={styles.textarea}
-            />
-          )}
-          {question.type === 'radio' && question.options && (
-            <div className={styles.radioContainer}>
-              {question.options.map((option) => (
-                <label key={option} className={styles.radioLabel}>
-                  <input
-                    {...getFieldProps(question.id, {
-                      type: 'radio',
-                      value: option,
-                    })}
-                  />
-                  {option}
-                </label>
-              ))}
-            </div>
-          )}
-          {question.type === 'checkbox' && question.options && (
-            <div>
-              {question.options.map((option) => (
-                <label key={option}>
-                  <input
-                    type='checkbox'
-                    {...getFieldProps(question.id, {
-                      type: 'checkbox',
-                      value: option,
-                    })}
-                    checked={(values[question.id] as string[])?.includes(
-                      option
-                    )}
-                  />
-                  {option}
-                </label>
-              ))}
-            </div>
-          )}
+  const renderQuestion = (question: SurveyQuestion) => {
+    const props = { question, getFieldProps, values, touched, errors };
+    switch (question.type) {
+      case 'text':
+        return <TextQuestion {...props} />;
+      case 'textarea':
+        return <TextareaQuestion {...props} />;
+      case 'radio':
+        return <RadioQuestion {...props} />;
+      case 'checkbox':
+        return <CheckboxQuestion {...props} />;
+      default:
+        return null;
+    }
+  };
 
-          {touched[question.id] && errors[question.id] && (
-            <p className={styles.errorText}>{errors[question.id]}</p>
-          )}
-        </div>
+  return questions.map((question) => (
+    <div key={question.id} className={styles.questionContainer}>
+      <label htmlFor={question.id} className={styles.questionTitle}>
+        {question.title}
+        {question.required && <span className={styles.requiredStar}>*</span>}
+      </label>
+      {renderQuestion(question)}
+      {touched[question.id] && errors[question.id] && (
+        <p className={styles.errorText}>{errors[question.id]}</p>
+      )}
+    </div>
+  ));
+}
+
+// ******
+
+interface QuestionProps {
+  question: SurveyQuestion;
+  getFieldProps: any;
+  values: Record<string, string | string[]>;
+  touched: Record<string, boolean>;
+  errors: Record<string, string>;
+}
+
+function TextQuestion({ question, getFieldProps }: QuestionProps) {
+  return (
+    <input
+      {...getFieldProps(question.id, { type: 'text' })}
+      className={styles.textInput}
+    />
+  );
+}
+
+function TextareaQuestion({ question, getFieldProps }: QuestionProps) {
+  return (
+    <textarea
+      {...getFieldProps(question.id, { type: 'textarea' })}
+      className={styles.textarea}
+    />
+  );
+}
+
+function RadioQuestion({ question, getFieldProps }: QuestionProps) {
+  return (
+    <div className={styles.radioContainer}>
+      {question.options?.map((option) => (
+        <label key={option} className={styles.radioLabel}>
+          <input
+            {...getFieldProps(question.id, {
+              type: 'radio',
+              value: option,
+            })}
+          />
+          {option}
+        </label>
       ))}
-    </>
+    </div>
+  );
+}
+
+function CheckboxQuestion({ question, getFieldProps, values }: QuestionProps) {
+  return (
+    <div className={styles.radioContainer}>
+      {question.options?.map((option) => (
+        <label key={option} className={styles.radioLabel}>
+          <input
+            type='checkbox'
+            {...getFieldProps(question.id, {
+              type: 'checkbox',
+              value: option,
+            })}
+            checked={(values[question.id] as string[])?.includes(option)}
+          />
+          {option}
+        </label>
+      ))}
+    </div>
   );
 }
