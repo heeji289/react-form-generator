b3c9e88392473d1ba6b11acba0f4eb272a223c54
limhizy15 <limhizy15@ajou.ac.kr>
2024-08-07 16:04:07 +0900

    chore: 불필요코드 삭제

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
 import App from './App';
 
-import { FormPreviewPage } from './app/form/form-preview/FormPreviewPage';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import FormSubmitPage from './app/form/form-submit/FormSubmitPage';
 import { SurveyPage } from './pages/survey/survey-response/SurveyPage';
 import { SurveyResultPage } from './pages/survey/survey-result/SurveyResultPage';
 
@@ -12,14 +10,6 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-  },
-  {
-    path: '/form/:id',
-    element: <FormPreviewPage />,
-  },
-  {
-    path: '/form/:id/submit',
-    element: <FormSubmitPage />,
   },
   {
     path: '/survey/:id',
